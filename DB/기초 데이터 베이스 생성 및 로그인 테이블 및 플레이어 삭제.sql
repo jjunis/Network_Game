bf46cd37f7831926e5924616cd3165ed677969c4
jjunis <owj1106@gmail.com>
2025-12-14 23:09:09 +0900

    [Feat] 데이터 테이블 오류 수정

--- DB/기초 데이터 베이스 생성 및 로그인 테이블 및 플레이어 삭제.sql
+++ DB/기초 데이터 베이스 생성 및 로그인 테이블 및 플레이어 삭제.sql
@@ -3,7 +3,7 @@ CREATE DATABASE `DevilRunDB` /*!40100 COLLATE 'utf8mb4_0900_ai_ci' */;
 USE devilrundb;
 
 -- 로그인 테이블
-devilrundbCREATE TABLE users (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE,
     password VARCHAR(255)
